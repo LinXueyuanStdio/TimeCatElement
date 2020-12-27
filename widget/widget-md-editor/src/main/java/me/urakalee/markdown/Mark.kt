691df1dd0178c652035ed23b5236c837585a3d1a
linxueyuanstdio <761516186@qq.com>
2020-12-27 23:46:03 +0800

    alert and listview

---
+++ widget/widget-md-editor/src/main/java/me/urakalee/markdown/Mark.kt
@@ -0,0 +1,34 @@
+package me.urakalee.markdown
+
+import me.urakalee.markdown.handler.*
+import me.urakalee.markdown.handler.NoneHandler
+import me.urakalee.markdown.handler.TodoHandler
+
+/**
+ * @author Uraka.Lee
+ */
+enum class Mark(val pattern: Regex, val defaultMark: String, val handler: MarkHandler) {
+
+    NONE(Regex(""), "", NoneHandler),
+    H(Regex("#+"), "#", HeaderHandler),
+    LI(Regex("[-*]"), "-", ListHandler),
+    LO(Regex("\\d\\."), "1.", ListHandler),
+    LA(Regex("[a-z]\\."), "a.", ListHandler),
+    TD(Regex("- \\[[x ]]", RegexOption.IGNORE_CASE), "- [ ]", TodoHandler),
+    QT(Regex(">"), ">", QuoteHandler);
+
+    companion object {
+
+        private fun fromString(s: String): Mark {
+            return values().firstOrNull {
+                s.matches(it.pattern)
+            } ?: NONE
+        }
+
+        fun handle(inputMark: Mark, source: String): String {
+            return Mark.fromString(source).let {
+                it.handler.handleMark(inputMark, source, it)
+            }
+        }
+    }
+}
